2f1129a8e125f4681527a60298cad1ed63b4d0dc
Ikerx0 <rap0zero26@gmail.com>
2025-05-22 19:26:05 +0200

    Mejora de comentarios en el codigo

--- app/src/main/java/com/example/taikaisensei/interfaz/pantallas/PantallaCompetidores.kt
+++ app/src/main/java/com/example/taikaisensei/interfaz/pantallas/PantallaCompetidores.kt
@@ -1,6 +1,5 @@
 package com.example.taikaisensei.interfaz.pantallas
 
-// Importaciones necesarias para la UI y estado en Jetpack Compose
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
